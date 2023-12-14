7be363253e614c4a9a0700f9ab06167aff3402d0
LiamEdwards931 <>
2023-12-14 11:27:57 +0000

    add z-index to js to bring the main content above the body, add favicon

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -3,4 +3,5 @@ function slideOff() {
     let mainContent = document.getElementById('main-content');
     landingPage.style.transform = 'translateY(-100vh)';
     mainContent.style.display = 'block';
+    mainContent.style.zIndex = '1';
 }
