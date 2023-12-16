356aaeed1a04da1da73a66f8f270fd93454ac369
LiamEdwards931 <>
2023-12-16 22:20:01 +0000

    made some changes with the timing in index.html for the screen to roll off, added the variable in the head of the page for JS to access, added navbar to base.html

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,11 +1,12 @@
-const Abouturl = "{% url 'about' %}"
-
 function slideOff() {
+    // Function that slides the landing page screen off vertically and loads the new URL
     let landingPage = document.getElementById('landing-page');
-    let mainContent = document.getElementById('main-content');
     landingPage.style.transform = 'translateY(-100vh)';
-    
+
+    // Access the about URL from the data attribute
+    const aboutUrl = document.documentElement.getAttribute('js-about-url');
+
     setTimeout(() => {
-        window.location.href = Abouturl
-    }, 1500);
+        window.location.href = aboutUrl;
+    }, 1000);
 }
