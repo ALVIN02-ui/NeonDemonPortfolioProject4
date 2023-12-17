d51ca0034cef5c3bdbbeae9bcc49f238f1051599
LiamEdwards931 <>
2023-12-17 11:53:28 +0000

    configured the {block tags} to extend from html, add the main content div in the about.html file for it to extend and start to style the content

--- NeonDemonSocialApp/urls.py
+++ NeonDemonSocialApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('base/', views.base, name='base'),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('base/', views.base, name='base')
 ]
