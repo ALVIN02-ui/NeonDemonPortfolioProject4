f553798efd1b1cf86aa03c87b170904a85cb3e08
LiamEdwards931 <>
2023-12-18 12:45:12 +0000

    finish style and script for the about-us section of the page, add function to make images jump to next

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,3 +1,4 @@
+// --------------------------------------------------------------------index.html JS
 function slideOff() {
     // Function that slides the landing page screen off vertically and loads the new URL
     let landingPage = document.getElementById('landing-page');
@@ -10,3 +11,35 @@ function slideOff() {
         window.location.href = aboutUrl;
     }, 1000);
 }
+
+// --------------------------------------------------------------------about.html JS
+document.addEventListener('DOMContentLoaded', function () {
+   
+    function jumpBetweenImages() {
+        //function that jumps between the images in the neon-img container.
+        const container = document.querySelector('.about-neon-img');
+        const images = container.querySelectorAll('.autoscroll');
+        let currentIndex = 0;
+        const jumpInterval = 3000; 
+
+        function jumpToNextImage() {
+            images[currentIndex].classList.add('hideimg'); 
+            currentIndex = (currentIndex + 1) % images.length; 
+            images[currentIndex].classList.remove('hideimg'); // Show the next image
+        }
+
+        setInterval(jumpToNextImage, jumpInterval); // Jump to the next image at regular intervals
+    }
+
+    jumpBetweenImages(); 
+});
+
+
+
+
+
+
+
+
+
+
