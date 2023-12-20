cc0ba9b779dd1e37513bf50217cb8f14c6919531
LiamEdwards931 <>
2023-12-20 12:52:59 +0000

    add 404 page in urls, and styled using a copy of the index.html file

--- NeonDemonSocialApp/urls.py
+++ NeonDemonSocialApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('base/', views.base, name='base'),
@@ -7,5 +8,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('social/', views.social, name='social'),
     path('reviews/', views.reviews, name='reviews'),
-    path('reviewform/', views.reviewform, name='reviewform')
+    path('reviewform/', views.reviewform, name='reviewform'),
+    handler404 = TemplateView.as_view(template_name='404.html')
 ]
