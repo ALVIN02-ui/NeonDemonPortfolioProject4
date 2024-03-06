f5da04b93d61b244ced7027d368fdb3cef338298
LiamEdwards931 <eddy-931@hotmail.co.uk>
2024-03-06 21:41:49 +0000

    fixed some bugs with the modal, display a log in and log out message on about.html, changed the CSS for the modals to respond better on mobile devices

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,10 @@
-from django.views import generic
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.contrib import messages
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.dispatch import receiver
 # Create your views here.
 
 
@@ -15,7 +14,17 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, f'Your account has been created {user.username}')
             return redirect("login")
     else:
         form = CustomUserCreationForm()
     return render(request, "registration/signup.html", {"form": form})
+
+@receiver(user_logged_in)
+def show_login_message(sender, user, request, **kwargs):
+    messages.info(request, f"Welcome, you are now logged in as {user.username}")
+
+@receiver(user_logged_out)
+def show_logout_message(sender, user, request, **kwargs):
+    if user is not None:
+        messages.info(request, f"Goodbye {user.username}, you have logged out")
